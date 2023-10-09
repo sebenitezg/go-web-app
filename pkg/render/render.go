8b5431df4565f41d78d683108577b4ca207e7bfb
Saul Benitez <sebenitezg@outlook.com>
2023-10-08 20:59:59 -0600

    Connected templates to routers

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"path/filepath"
 
+	"github.com/justinas/nosurf"
 	"github.com/sebenitezg/go-web-app/pkg/config"
 	"github.com/sebenitezg/go-web-app/pkg/models"
 )
@@ -20,12 +21,13 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
 	/* Add default data to the template data */
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *models.TemplateData) {
 	/* Render HTML templates */
 
 	var tc map[string]*template.Template
@@ -43,7 +45,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buff := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	err := t.Execute(buff, td)
 	if err != nil {
